38356c6ff1edc69d3930e4825056ffb3be3856a0
Josh <admin@lipsmoker.com>
2023-02-15 19:27:41 +0000

    Paused Progress

--- src/main/java/com/noobcore/noobcorecounter/commands/FiftyPartyStart.java
+++ src/main/java/com/noobcore/noobcorecounter/commands/FiftyPartyStart.java
@@ -17,42 +17,44 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class FiftyPartyStart implements CommandExecutor {
 
-    public static BukkitTask currentRunnable = null;
+    private static BukkitTask currentRunnable = null;
     private static ArmorStand armorStand = null;
 
     @Override
     public boolean onCommand(@NonNull CommandSender sender, @NonNull Command command, @NonNull String label, String[] args) {
 
         // Check if the task is already running and stop further logic if it is
-        if (currentRunnable != null && !currentRunnable.isCancelled()) {
+        if (isTaskRunning()) {
             sender.sendMessage("You cannot activate this party when an existing party is running!");
             return true;
         }
 
-        if (TwentyPartyStart.currentRunnable != null && !TwentyPartyStart.currentRunnable.isCancelled()) {
+        if (TwentyPartyStart.isTaskRunning()) {
             sender.sendMessage("You cannot activate this party when an existing party is running!");
             return true;
+
         }
 
 
+            //Desired Location -177.500, 53, 232.500 World name 'newlobby'
+            World world = Bukkit.getWorld("newlobby");
+            armorStand = (ArmorStand) world.spawnEntity(new Location((world), -177.500, 53, 232.500), EntityType.ARMOR_STAND);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "discord broadcast #585953624530616320 :information_source:`A gem party of 50 is starting in 5 minutes! Hop online and type /party to join!`");
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "infiniteannouncements send * dropparty");
+            //set stand attributes
+            armorStand.setGravity(false);
+            armorStand.setInvulnerable(true);
+            armorStand.setCustomNameVisible(true);
+            armorStand.setVisible(false);
+            armorStand.customName(Component.text("5:00"));
 
+            this.currentRunnable = new FiftyPartyTimer(armorStand, 300).runTaskTimer(Noobcorecounter.instance, 0, 20);
 
+            return true;
+        }
 
-        //Desired Location -177.500, 53, 232.500 World name 'newlobby'
-        World world = Bukkit.getWorld("newlobby");
-        armorStand = (ArmorStand) world.spawnEntity(new Location((world), -177.500, 53, 232.500), EntityType.ARMOR_STAND);
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "discord broadcast #585953624530616320 :information_source:`A gem party of 50 is starting in 5 minutes! Hop online and type /party to join!`");
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "infiniteannouncements send * dropparty");
-        //set stand attributes
-        armorStand.setGravity(false);
-        armorStand.setInvulnerable(true);
-        armorStand.setCustomNameVisible(true);
-        armorStand.setVisible(false);
-        armorStand.customName(Component.text("5:00"));
-
-        this.currentRunnable = new FiftyPartyTimer(armorStand, 300).runTaskTimer(Noobcorecounter.instance, 0, 20);
-
-        return true;
+        public static boolean isTaskRunning() {
+            return currentRunnable != null && !currentRunnable.isCancelled();
+        }
     }
-
 }
