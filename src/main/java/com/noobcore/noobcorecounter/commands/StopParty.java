af4ecac42b0c7b5f4b45e90dd6da8f88d0882aa4
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2023-02-15 18:13:39 +0100

    Add some comment tips

--- src/main/java/com/noobcore/noobcorecounter/commands/StopParty.java
+++ src/main/java/com/noobcore/noobcorecounter/commands/StopParty.java
@@ -7,6 +7,10 @@ import org.jetbrains.annotations.NotNull;
 
 public class StopParty implements CommandExecutor {
 
+    /**
+     * TODO convert BukkitTasks from 20 and 50 into static, reference them here.
+     * TODO Check if they're running. If yes, cancel them
+     */
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
